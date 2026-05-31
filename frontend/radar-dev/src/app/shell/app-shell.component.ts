4d24710abe3c1076ede0301c88b3ae6c936e6124
Eduardo Fontenelle <efcarrera@gmail.com>
2026-05-31 12:35:58 -0300

    feat(frontend): add board config status indicator to AppShell

--- frontend/radar-dev/src/app/shell/app-shell.component.ts
+++ frontend/radar-dev/src/app/shell/app-shell.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from '../core/services/user.service';
+import { BoardConfigService } from '../core/services/board-config.service';
 import { UserIdentificationModalComponent } from '../core/components/user-identification-modal/user-identification-modal.component';
 import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-shell',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, UserIdentificationModalComponent],
+  imports: [RouterOutlet, RouterLink, CommonModule, UserIdentificationModalComponent],
   templateUrl: './app-shell.component.html',
   styleUrl: './app-shell.component.scss'
 })
@@ -17,12 +18,18 @@ export class AppShellComponent implements OnInit {
   showModal = false;
   userName = '';
   connectionStatus: 'connecting' | 'connected' | 'disconnected' = 'connecting';
+  isBoardConfigured = false;
 
-  constructor(private userService: UserService, private http: HttpClient) {}
+  constructor(
+    private userService: UserService,
+    private boardConfigService: BoardConfigService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.showModal = !this.userService.hasUser();
     this.userName = this.userService.getUserName() ?? '';
+    this.isBoardConfigured = this.boardConfigService.hasConfig();
     this.http.get(`${environment.apiUrl}/health`).subscribe({
       next: () => this.connectionStatus = 'connected',
       error: () => this.connectionStatus = 'disconnected'
